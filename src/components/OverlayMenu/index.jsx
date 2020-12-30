2f27fdca2523b77cda8a79a7455a556708f6c564
Babak <babakjahangiri@gmail.com>
2020-12-30 05:52:45 +0000

    update Overlay Menu

--- src/components/OverlayMenu/index.jsx
+++ src/components/OverlayMenu/index.jsx
@@ -1,49 +1,48 @@
-import React, { useState,useEffect, useContext } from 'react';
-import { NavLink } from "react-router-dom";
-import {ToggleBurgerContext,ToggleBurgerProvider} from "./../../context";
-import './overlay-menu.scss';
+import React, { useState, useEffect, useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { ToggleBurgerContext } from './../../context'
+import './overlay-menu.scss'
 
 const OverlayMenu = () => {
+    const [burgerStatus] = useContext(ToggleBurgerContext)
+    const [menuClass, setMenuClass] = useState('')
 
-const [burgerStatus,setBurgerStatus]= useContext(ToggleBurgerContext);
-const [menuClass,setMenuClass] = useState('');
+    useEffect(() => {
+        if (burgerStatus) {
+            setMenuClass('overlay-menu-reveal')
+        } else {
+            setMenuClass('')
+        }
+    }, [burgerStatus])
 
-  useEffect(() => {
-    if (burgerStatus){
-      setMenuClass('overlay-menu-reveal');
-    }else{
-      setMenuClass('');
-    }
+    return (
+        <div className={`overlay-menu ${menuClass}`}>
+            <nav className="nav-hamburger">
+                <ul>
+                    <li>
+                        <NavLink to="/about" exact>
+                            About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/projects" exact>
+                            Projects
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/solutions" exact>
+                            Solutions
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact" exact>
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
 
-  }, [burgerStatus])
-
-
-  return (
-  <div className={`overlay-menu ${menuClass}`}>
-        <nav className="nav-hamburger">
-          <ul>
-            <li>
-            <NavLink to="/about" exact>About</NavLink>
-            </li>
-            <li>
-            <NavLink to="/projects" exact>Projects</NavLink>
-            </li>
-            <li>
-             <NavLink to="/solutions" exact>
-        Solutions
-      </NavLink></li>
-      <li>
-      <NavLink to="/contact" exact>
-        Contact
-      </NavLink></li>
-          </ul>
-        </nav>
-  </div>
-  );
-};
-
-
-      
-     
-
-export default OverlayMenu;
+export default OverlayMenu
