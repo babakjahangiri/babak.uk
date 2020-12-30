26d089cd057d307148d077276f891a66d97d8c36
Babak <babakjahangiri@gmail.com>
2020-12-30 05:54:49 +0000

    fix styles

--- src/components/PageHeaderRight/index.jsx
+++ src/components/PageHeaderRight/index.jsx
@@ -1,53 +1,53 @@
-import React from "react";
-import "./PageHeaderRight.scss";
-import { motion } from "framer-motion";
-import aboutImg from "./../../images/about.jpg";
-import solutionsImg from "./../../images/solutions.jpg";
+import React from 'react'
+import './PageHeaderRight.scss'
+import { motion } from 'framer-motion'
+import aboutImg from './../../images/about.jpg'
+import contactImg from './../../images/contact.jpg'
+import projectsImg from './../../images/projects.jpg'
+import solutionsImg from './../../images/solutions.jpg'
 
 const PageHeaderRight = ({ title }) => {
-  let headerImg = "";
-  switch (title.toLowerCase()) {
-    case "about":
-      headerImg = aboutImg;
-      break;
-    case "contact":
-      headerImg = aboutImg;
-      break;
-    case "projects":
-      headerImg = aboutImg;
-      break;
-    case "solutions":
-      headerImg = solutionsImg;
-      break;
-    default:
-      headerImg = aboutImg;
-      break;
-  }
+    let headerImg = ''
+    switch (title.toLowerCase()) {
+        case 'about':
+            headerImg = aboutImg
+            break
+        case 'contact':
+            headerImg = contactImg
+            break
+        case 'projects':
+            headerImg = projectsImg
+            break
+        case 'solutions':
+            headerImg = solutionsImg
+            break
+        default:
+            headerImg = projectsImg
+            break
+    }
 
-  //const HeaderImgOpacity = useTransform(scrollY, [400, 500], [0.1, 1]);
+    return (
+        <motion.div
+            className="pageheader__rightContainer"
+            initial={{ opacity: -1 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+        >
+            <div>
+                <img
+                    src={headerImg}
+                    alt="Babak Jahangiri"
+                    className="pageheader-image"
+                />
+            </div>
+            <div>
+                <p>{title}</p>
+                <div>
+                    <p className="header__right-downblock"></p>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
 
-  return (
-    <motion.div
-      className="pageheader__rightContainer"
-      initial={{ opacity: -1 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-    >
-      <div>
-        <img
-          src={headerImg}
-          alt="Babak Jahangiri"
-          className="pageheader-image"
-        />
-      </div>
-      <div>
-        <p>{title}</p>
-        <div>
-          <p className="header__right-downblock"></p>
-        </div>
-      </div>
-    </motion.div>
-  );
-};
-
-export default PageHeaderRight;
+export default PageHeaderRight
