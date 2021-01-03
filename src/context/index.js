a4ad6e4b633a23a00682c3513ed08c42c4882203
Babak <babakjahangiri@gmail.com>
2021-01-03 05:13:18 +0000

    add project filter context

--- src/context/index.js
+++ src/context/index.js
@@ -1,10 +1,23 @@
-import React, { useState, createContext } from "react";
-export const ToggleBurgerContext = createContext();
+import React, { useState, createContext } from 'react'
+export const ToggleBurgerContext = createContext()
+export const ProjectsFilterContext = createContext()
 
 export const ToggleBurgerProvider = (props) => {
-  const [burgerStatus,setBurgerStatus] = useState(false);
-return (
-<ToggleBurgerContext.Provider value={[burgerStatus,setBurgerStatus]}>
-{props.children}
-</ToggleBurgerContext.Provider>
-)};
+    const [burgerStatus, setBurgerStatus] = useState(false)
+    return (
+        <ToggleBurgerContext.Provider value={[burgerStatus, setBurgerStatus]}>
+            {props.children}
+        </ToggleBurgerContext.Provider>
+    )
+}
+
+export const ProjectsFilterProvider = (props) => {
+    const [projectFilter, setProjectFilter] = useState('')
+    return (
+        <ProjectsFilterContext.Provider
+            value={[projectFilter, setProjectFilter]}
+        >
+            {props.children}
+        </ProjectsFilterContext.Provider>
+    )
+}
