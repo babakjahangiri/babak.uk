b43534b29f1c9b99b3134da1178439b5daaa9316
Babak <babakjahangiri@gmail.com>
2021-01-03 04:30:41 +0000

    add content

--- src/components/Contact/ThanksContact.jsx
+++ src/components/Contact/ThanksContact.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export const ThanksContact = () => {
-    return <div>Thank you for you contact</div>
+    return (
+        <div className="full-section">
+            <h2 className="m-title">Thank you for you contact</h2>
+            <p>
+                I appreciate you contacting me. I will get back in touch with
+                you soon!
+            </p>
+        </div>
+    )
 }
 
 export default ThanksContact
