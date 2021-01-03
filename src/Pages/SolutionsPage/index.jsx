6663980a16e18182501112c024233ab100100bb6
Babak <babakjahangiri@gmail.com>
2021-01-03 04:31:33 +0000

    add content

--- src/Pages/SolutionsPage/index.jsx
+++ src/Pages/SolutionsPage/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Header } from './../../components'
-
+import { Solutions } from './../../components/'
 const SolutionsPage = () => {
     return (
         <React.Fragment>
             <Header />
-            <h1>This Page is under construction</h1>
-            <div>This is Solution Page </div>;
+            <Solutions />
         </React.Fragment>
     )
 }
